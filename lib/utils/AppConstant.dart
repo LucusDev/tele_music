986f8fc0617e4b0e18a43a296dd720f979f9b562
LucusDev <htetaungshine20028@gmail.com>
2023-10-15 19:24:16 +0700

    fixed ios, changed url

--- lib/utils/AppConstant.dart
+++ lib/utils/AppConstant.dart
@@ -2,7 +2,8 @@ class AppConstant {
   //app name
   static const String appName = 'TeleMusic';
 
-  static const String BaseUrl = "https://telemusic.io" + "/api/" + "v1/";
+  static const String BaseUrl =
+      "https://telemusic.io" + "/public" + "/api/" + "v1/";
   static const String ImageUrl =
       "https://telemusic.io/public/"; //main web url or base url here
 
