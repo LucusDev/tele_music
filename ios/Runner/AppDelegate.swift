4b79379d1845dece6e1009c8790c5e91e56ba750
LucusDev <htetaungshine20028@gmail.com>
2024-02-08 16:12:35 +0700

    latest updated

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMobileAds
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GADMobileAds.sharedInstance().requestConfiguration.testDeviceIdentifiers = [ kGADSimulatorID ]
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- lib/utils/AdHelper.dart
+++ lib/utils/AdHelper.dart
@@ -3,9 +3,9 @@ import 'dart:io';
 class AdHelper {
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
-      return 'ca-app-pub-22920965380/202400';
+      return '/22920965380/202400';
     } else if (Platform.isIOS) {
-      return 'ca-app-pub-22920965380/202400';
+      return '/22920965380/202400';
     } else {
       throw UnsupportedError('Unsupported platform');
     }
@@ -13,9 +13,9 @@ class AdHelper {
 
   static String get interstitialAdUnitId {
     if (Platform.isAndroid) {
-      return "ca-app-pub-22920965380/202411";
+      return "/22920965380/202411";
     } else if (Platform.isIOS) {
-      return "ca-app-pub-22920965380/202411";
+      return "/22920965380/202411";
     } else {
       throw UnsupportedError("Unsupported platform");
     }
